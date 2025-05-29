7cdf0fbf3654e0ad10c681476d67d77c4debb89a
Marcus Zucareli <papaulozucareli@gmail.com>
2025-05-29 09:01:11 -0300

    docs: create docstrings for Mlflow client API file

--- mlflow_client/client.py
+++ mlflow_client/client.py
@@ -1,13 +1,42 @@
+"""
+client.py
+
+This module defines clients for interacting with the MLflow API. Includes:
+
+- APIClientInterface: Abstract base class defining the interface and common
+errors.
+- MlflowAPI: Client for interacting with the production MLflow API.
+- MockAPI: Mock client used for development and testing.
+
+Author: Marcus Zucareli
+Date: 2025-05-29
+"""
+
 import os
 import requests
 import json
+from mlflow.entities.model_registry import RegisteredModel
+
 
 class APIClientInterface:
-    def get_models(self) -> list:
-        raise NotImplementedError('get_models not implemented')
+    def get_models(self) -> list[RegisteredModel]:
+        """
+        Fetch registered models from the Mlflow API.
+
+        Raises:
+            NotImplementedError: This method must be overridden in subclasses.
+        """
+        raise NotImplementedError('get_models not implemented in this class.')
 
 class MlflowAPI(APIClientInterface):
-    def get_models(self) -> list:
+    def get_models(self) -> list[RegisteredModel]:
+        """
+        Fetch registered models from the Mlflow API.
+
+        Returns:
+            List[RegisteredModel]: A list of registered models fetched from the
+            MLflow API.
+        """
         API_BASE_URL = os.getenv('API_BASE_URL')
         url = f'{API_BASE_URL}'
         res = [url]
@@ -16,7 +45,7 @@ class MlflowAPI(APIClientInterface):
 class MockAPI(APIClientInterface):
     def __init__(self):
         import time
-        from mlflow.entities.model_registry import RegisteredModel, RegisteredModelTag
+        from mlflow.entities.model_registry import RegisteredModelTag
 
         self._mock_model_1 = RegisteredModel(
             name="mock_model",
@@ -45,5 +74,12 @@ class MockAPI(APIClientInterface):
         )
 
 
-    def get_models(self) -> list:
+    def get_models(self) -> list[RegisteredModel]:
+        """
+        Simulate the `get_models` method of MlflowAPI for development and
+        testing purposes.
+
+        Returns:
+            list[RegisteredModel]: A list of mocked MLflow registered models.
+        """
         return [self._mock_model_1, self._mock_model_2]
