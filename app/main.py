e9961eaff927bc9741425a2722bf554a1e86991e
Marcus Zucareli <papaulozucareli@gmail.com>
2025-05-29 16:55:06 -0300

    feat: Add country and city selection in streamlit
    - Add link from streamlit API to get models
    - Add country and city selection
    - Add error handler in no models case
    - Add error page

--- app/main.py
+++ app/main.py
@@ -6,12 +6,62 @@ from mlflow_client.registry import get_places
 def get_locations():
     return get_places()
 
+
+def update_city():
+    print('Update Cidade:')
+    print(st.session_state['city'])
+
+
+# Get list of available places
 places = get_locations()
 
+# Evaluate places output
+if not places or not isinstance(places, dict):
+    # st.error('error')
+    a, b = st.columns(2, vertical_alignment='center')
+    with a:
+        st.markdown(
+            """
+            # We are sorry...
+
+            It looks like our servers are not working right now.
+
+            Try to refresh the page in a few moments.
+            """)
+    with b:
+        st.image("https://cdn-icons-png.flaticon.com/512/9068/9068699.png")
+    
+    st.markdown("---")
+    st.html(
+        '<a href="https://www.flaticon.com/free-icons/close" title="close icons">Close icons created by Fathema Khanom - Flaticon</a>'
+    )
+    
+    st.stop()
+
+# Add empty selection to the countries options list
+countries = [''] + list(places.keys())
+
+# Initiate city var
+if "city" not in st.session_state:
+    st.session_state.city = ''
+
+# Welcome message
 st.markdown("""
 # Welcome to the World Real Estate Evaluator!
 
-In this app, you can evaluate properties from various cities.
+In this app, you can evaluate properties from various cities around the world!
+            
+Start by choosing a **country**.
 """)
 
-st.selectbox('Country', places.keys())
+# Country selectbox
+st.selectbox('Country', countries, key='country')
+
+# City selectbox
+if st.session_state['country'] != '':
+    st.markdown('Now choose a **city**')
+    st.selectbox('City', [''] + places[st.session_state['country']], on_change=update_city, key='city')
+
+# Input model parameters
+if st.session_state['city'] != '':
+    st.markdown('Now fill the details about the property you want to evaluate bellow.')
