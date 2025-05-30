894978e9bc6359c6d7548e1a55185b0ac3b83d0b
Marcus Zucareli <papaulozucareli@gmail.com>
2025-05-30 19:23:31 -0300

    refactor: Change code base structure

--- tests/mlflow_client/test_registry.py
+++ tests/mlflow_client/test_registry.py
@@ -1,12 +1,13 @@
 import pytest
-from mlflow_client.registry import get_places
+from mlflow_client.registry import Models
 
 def test_get_places(monkeypatch):
     monkeypatch.setenv('ENV', 'dev')
-    places = get_places()
+    models = Models()
+    places = models.get_places()
 
     assert isinstance(places, dict)
     assert isinstance(places['Brazil'], list)
     assert places['Brazil'] == ["Jacareí", "São José dos Campos", "Taubaté"]
-    assert places['France'] == ["Paris"]
+    assert places['France'] == ["Lyon", "Paris"]
     assert list(places.keys()) == ['Brazil', 'France']
