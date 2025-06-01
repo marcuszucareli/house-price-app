42d4d2782aac7c668acbc76bb552e7171c5d0acf
Marcus Zucareli <papaulozucareli@gmail.com>
2025-06-01 20:45:27 -0300

    feat: Add model logging class
    - Define Inputs class, necessary for model logging
    - Define ModelLogInput class to help evaluate data
    to be logged in mlflow
    - Define test for both classes
    - Add streamlit config files for dev and prod

---
+++ mlflow_client/model_logging.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass, field
+from typing import Optional
+from datetime import datetime
+
+# Define the maximum number of chars in strings
+max_chars = 64
+
+@dataclass
+class Inputs:
+    """
+    Defines the necessary parameters that each input of the model should have.
+
+    Args:
+        column_name (str): The column name in the model.
+        label (str): The name to be displayed in the Streamlit app.
+        type (str): The type of the input. Must be one of the following
+                    options:
+            - "bool":  A boolean parameter.
+            - "int": An int parameter.
+            - "float": An float parameter.
+            - "categorical": A categorical parameter. If choosing "categorical"
+                you must specify the options attribute.
+        options (list[str]): The options of the categorical parameter.
+        description (Optional[str] = None): A brief description of the 
+        parameter.
+        unit (Optional[str] = None): Unit of measurement associated with the 
+        parameter.
+    """
+    column_name: str
+    label: str
+    type: str
+    options: Optional[list[str]] = field(default_factory=list)
+    description: Optional[str] = None
+    unit: Optional[str] = None
+
+
+    def __post_init__(self):
+        allowed_data_types = ['bool', 'int', 'float', 'categorical']
+        if len(self.column_name) > max_chars:
+            raise ValueError("column_name cannot be longer than 64 characters")
+        
+        if len(self.label) > max_chars:
+            raise ValueError("label cannot be longer than 128 characters")
+        
+        if self.type not in allowed_data_types:
+            error_message = "type must be one of the following strings"
+            data_types = ', '.join(allowed_data_types)
+            raise ValueError(f"{error_message} {data_types}")
+        
+        if self.type == 'categorical':
+            if len(self.options) < 2:
+                raise ValueError(
+                    "Categorical variables require the available " \
+                    "options to be defined.")
+        
+        if isinstance(self.description, str) and \
+            len(self.description) > 200:
+            
+            raise ValueError("label cannot be longer than 128 characters")
+        
+        if isinstance(self.unit, str) and \
+            len(self.unit) > max_chars:
+            raise ValueError("label cannot be longer than 128 characters")
+
+
+@dataclass
+class ModelLogInput:
+    """
+    Validates model information to ensure compliance with the project's logging
+     standards.
+    
+    Args:
+        model (any): A machine learning model compatible with the MLflow 
+        library.
+        mape (float | int): Mean Absolute Percentage Error of the model in 
+        decimal form.
+        mae (float | int): Mean Absolute Error of the model.
+        rmse (float | int): Root Mean Squared Error of the model.
+        r2 (float | int): R-squared of the model
+        x_test (pd.DataFrame): A sample of 100 rows from the model's test
+         data, used for validation and logging.
+        y_predict (pd.Series | np.ndarray): The prediction associated with the
+         x_test data, used for validation and logging..
+        author (str): The author of the model
+        algorithm (str): The algorithm used to train the model (e.g., linear 
+        regression, random forest, XGBoost, etc.).
+        data_year (int): The earliest year of the data used to train the model.
+        country (str): The country associated with the data. Must follow the 
+        ISO 3166 country name standard.
+        See: https://www.iso.org/iso-3166-country-codes.html
+        cities (list[str]): A list of cities associated with the data.
+        inputs (list[Inputs]): A list of user-provided inputs required to make 
+        a prediction. Do not include feature engineering parameters—this list 
+        should contain only the inputs explicitly required from the user.
+    """
+    # Model
+    model: any
+
+    # Metrics
+    mape: float | int
+    mae: float | int
+    rmse: float | int
+    r2: float | int
+
+    # Artefacts
+    x_test: pd.DataFrame
+    y_predict: pd.Series | np.ndarray
+
+    # Tags
+    author: str
+    algorithm: str
+    data_year: int
+    country: str
+    cities: list[str]
+    inputs: list[Inputs]
+
+
+    def __post_init__(self):
+        # Validate model
+        if self.model is None:
+            raise TypeError("You need to provide a model.")
+        # Validate mape decimal form
+        if self.mape < 0 or self.mape > 1:
+            raise ValueError("MAPE must be provided in decimal form.")
+        # Validate x_test
+        if len(self.x_test) != 100:
+            raise ValueError("x_test must have 100 lines")
+        # Validate y_test
+        if len(self.y_predict) != 100:
+            raise ValueError("y_predict must have 100 lines")
+        # Validate R²
+        if self.r2 < -1 or self.r2 > 1:
+            raise ValueError("R² value must be between -1 and 1.")
+        # Validate data_year
+        if self.data_year > datetime.now().year:
+            raise ValueError("data_year must not exceed the current year.")
+        # Validate inputs
+        for input in self.inputs:
+            if input.column_name not in self.x_test.columns.to_list():
+                raise ValueError(f"{input.column_name} is not a x_test column")
+        
---
+++ tests/mlflow_client/test_model_logging.py
@@ -0,0 +1,172 @@
+import pytest
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from sklearn.linear_model import LinearRegression
+from mlflow_client.model_logging import Inputs, ModelLogInput, max_chars
+
+# Define base model input
+base_ModelLogInput = {
+    'model': LinearRegression(),
+    'mape': 1,
+    'mae': 42,
+    'rmse': 42,
+    'r2': -1,
+    'x_test': pd.DataFrame(
+        {
+            "column_1": [i for i in range(100)],
+            "column_2": [i for i in range(100)]
+        }
+    ),
+    'y_predict': np.array([i for i in range(100)]),
+    'author': 'Edson Arantes do Nascimento',
+    'algorithm': 'regression',
+    'data_year': 2025,
+    'country': 'Brazil',
+    'cities': ['Belo Horizonte', 'Contagem'],
+    'inputs': [
+        Inputs(
+            **{
+                'column_name': "column_1",
+                'label': "Is your house new?",
+                'type': "bool",
+                'options': [],
+                'description': 'If you house has less than 5 years',
+                'unit': None
+            }
+        )
+    ]
+}
+
+# Define the wrong input class
+base_Inputs = {
+    'column_name': "is_new",
+    'label': "Is your house new?",
+    'type': "bool",
+    'options': [],
+    'description': 'If you house has less than 5 years',
+    'unit': None
+}
+wrong_input = base_Inputs.copy()
+wrong_input['column_name'] = 'Not in x_test'
+
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [   
+        # Type bool
+        (
+            {
+                'column_name': "is_new",
+                'label': "Is your house new?",
+                'type': "bool",
+                'options': [],
+                'description': 'If you house has less than 5 years',
+                'unit': None
+            },
+            Inputs
+        ),
+        # Type int
+        (
+            {
+                'column_name': "n_bedrooms",
+                'label': "Number of bedrooms in the house",
+                'type': "int",
+                'options': [],
+                'description': '',
+                'unit': 'un'
+            },
+            Inputs
+        ),
+        # Type float
+        (
+            {
+                'column_name': "m2",
+                'label': "area_m2",
+                'type': "float",
+                'options': [],
+                'description': 'The property size in m².',
+                'unit': 'm²'
+            },
+            Inputs
+        ),
+        # Type categorical
+        (
+            {
+                'column_name': "neighbourhood",
+                'label': "Neighbourhood",
+                'type': "categorical",
+                'options': ['Morumbi', 'América'],
+                'description': 'Property neighbourhood',
+                'unit': None
+            },
+            Inputs
+        )
+    ]
+)
+def test_Inputs_validation_success(input_data, expected):
+    model_input = Inputs(**input_data)
+    assert isinstance(model_input, expected)
+
+
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [   
+        # column_name string size
+        ({'column_name': "a"*(max_chars+1)}, ValueError),
+        # label string size
+        ({'label': "a"*(max_chars+1)}, ValueError),
+        # not listed type
+        ({'type': "a"}, ValueError),
+        # Only one option
+        ({'options': ['Morumbi'],
+          'type': 'category'
+          }, ValueError),
+        # Description string size
+        ({'description': "a"*250}, ValueError),
+        # unit string size
+        ({'unit': "a"*(max_chars+1)}, ValueError)
+    ]
+)
+def test_Inputs_validation_error(input_data, expected):
+    model_inputs = base_Inputs.copy()
+    for item, value in input_data.items():
+        model_inputs[item] = value
+
+    with pytest.raises(expected):
+        Inputs(**model_inputs)
+
+
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [
+        (base_ModelLogInput, ModelLogInput)
+    ]
+)
+def test_ModelLogInput_success(input_data, expected):
+    model_log_input = ModelLogInput(**input_data)
+    assert isinstance(model_log_input, ModelLogInput)
+
+
+@pytest.mark.parametrize(
+    'input_data, expected',
+    [
+        ({'model': None}, TypeError),   # No model
+        ({'mape': 2}, ValueError),   # Mape > 1
+        ({'mape': -2}, ValueError),   # Mape < -1
+        ({'x_test': pd.DataFrame( \
+            {"Column_1": [1]})}, ValueError),   # x_test size != 100
+        ({'y_predict': np.array([1])}, ValueError),   # y_predict size != 0
+        ({'r2': -2}, ValueError),   # R² < -1
+        ({'r2': 2}, ValueError),   # R² > 1
+        ({'data_year': \
+          datetime.now().year + 1}, ValueError),   # date_year > current year
+        ({'inputs': [Inputs(**wrong_input)]}, ValueError)   # Wrong input
+    ]
+)
+def test_ModelLogInput_error(input_data, expected):
+    model_inputs = base_ModelLogInput.copy()
+    for item, value in input_data.items():
+        model_inputs[item] = value
+
+    with pytest.raises(expected):
+        ModelLogInput(**model_inputs)
